8fa00f4469d70ef9468bfcb0350c0c9094bb3f17
arch <gustavopaivads@gmail.com>
2025-09-10 13:48:50 -0300

    menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Roboto } from 'next/font/google';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../src/theme';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-
+import  Header from "../src/menu.tsx";
 
 const roboto = Roboto({
     weight: ['300', '400', '500', '700'],
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="pt-BR" className={roboto.variable}>
     <body>
         <AppRouterCacheProvider>
+            <Header />
             <ThemeProvider theme={theme}>
                 {children}
             </ThemeProvider>
